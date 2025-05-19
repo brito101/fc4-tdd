f0d6955a0edd96371366ecae5486601b8d17691d
brito101 <rodrigobrito101@hotmail.com>
2025-05-19 09:38:33 -0300

    first commit

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,27 @@
+export class User {
+  private readonly id: string;
+  private readonly name: string;
+  constructor(id: string, name: string) {
+    this.validateUser(id, name);
+    this.id = id;
+    this.name = name;
+  }
+
+  private validateUser(id: string, name: string): void {
+    if (!name) {
+      throw new Error("O nome é obrigatório");
+    }
+
+    if (!id) {
+      throw new Error("O ID é obrigatório");
+    }
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+}
